21f649591e87bead5712fb6a0fde5d5e58fb9ac5
Chris Towles <Chris.Towles@gmail.com>
2024-11-23 16:28:02 -0500

    chore: wip embedings working

--- ReadMe.md
+++ ReadMe.md
@@ -82,3 +82,12 @@ docker compose down --volumes
 
 - https://github.com/phidatahq/phidata/blob/main/cookbook/assistants/llms/groq/rag/README.md?plain=1
 - https://github.com/ChingWeiChan/ollama-streamlit-demo/blob/main/main.py
+- https://github.com/Azure-Samples/rag-postgres-openai-python
+- https://electric-sql.com/blog/2024/02/05/local-first-ai-with-tauri-postgres-pgvector-llama
+- https://docs.llamaindex.ai/en/stable/getting_started/starter_tools/rag_cli/
+- https://github.com/HamedMP/NextRag
+- https://www.theunwindai.com/p/build-a-local-rag-agent-with-llama-3-2-and-vector-database?s=31
+- https://www.inferable.ai/blog/posts/sqlite-rag
+- https://docs.mistral.ai/guides/rag/#:~:text=Split%20document%20into%20chunks%E2%80%8B,and%20we%20get%2037%20chunks
+- https://www.timescale.com/blog/postgresql-as-a-vector-database-create-store-and-query-openai-embeddings-with-pgvector/
+  - has some good chunking code
--- area/postgres/dataset/00_init.sql
+++ area/postgres/dataset/00_init.sql
@@ -1,16 +1,31 @@
 
-CREATE TABLE books (
-    "ISBN" TEXT,
-    "Book-Title" TEXT,
-    "Book-Author" TEXT,
-    "Year-Of-Publication" TEXT, 
-    "Publisher" TEXT,
-    "Image-URL-S" TEXT,
-    "Image-URL-M" TEXT,
-    "Image-URL-L" TEXT
+CREATE TABLE text_encodings (
+    text_encodings_id SERIAL PRIMARY KEY,
+    model_name varchar,
+    input_text TEXT NOT NULL,
+    encoded_text BYTEA NOT NULL,
+    embedding vector(1536)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
+
+# Create table to store embeddings and metadata
+table_create_command = """
+CREATE TABLE embeddings (
+            id bigserial primary key, 
+            title text,
+            url text,
+            content text,
+            tokens integer,
+            embedding vector(1536)
+            );
+            """
+
+cur.execute(table_create_command)
+cur.close()
+conn.commit()
 
 
 -- Import data from CSV file
